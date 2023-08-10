dfc3534b72bed347595039314397b14677125c4c
Pedro Ramos Leite Soares <91470099+pedrorl11@users.noreply.github.com>
2023-08-10 11:51:09 -0300

    Add files via upload

---
+++ Progweb-2023/php/exercicio1/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Exercício 1</title>
+</head>
+
+<body>
+<?php
+echo "<img src='https://picsum.photos/500/600'><br>";
+echo "<h1> Imagem da semana 2 </h1>";
+
+?>
+</body>
+</html>
---
+++ Progweb-2023/php/exercicio2/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Exercício 1</title>
+</head>
+
+<body>
+<?php
+echo "<h1> URLs Absolutas </h1><br>";
+echo "<a href='https://www.w3schools.com/'>W3C</a><br>";
+echo "<a href='https://www.google.com.br/?hl=pt-BR'>Google</a><br>";
+
+echo "<h1> URLs relativas </h1><br>";
+echo "<a href='imagens.php'>HTML Images</a><br>";
+echo "<a href='tutorialcss.php'>CSS Tutorial</a><br>";
+
+?>
+</body>
+</html>
